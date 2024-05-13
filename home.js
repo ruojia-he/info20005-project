170c12bf1fad5fcc136083f89e242c8b491ce53e
Ruojia He <187375532+ruojia-he@users.noreply.github.com>
2024-05-13 22:28:43 +1000

    msg

--- home.js
+++ home.js
@@ -17,9 +17,53 @@ const closeButton = document.querySelector('.sidebar-close');
 closeButton.addEventListener('click', () => {overlayOff()});
 
 
-/* total-purchase */
+/* total-purchase 
 const totalPurchaseBtn = document.getElementById("total-purchase-btn");
 
 function btnActive() {
     totalPurchaseBtn.classList.add('active');
+} */
+
+
+/* checkout steps */
+let stepOne = document.getElementById("step-one-show");
+let isStepOne = stepOne.classList.contains("step-one-show");
+
+let stepTwo = document.getElementById("step-two-show");
+let isStepTwo  = stepTwo.classList.contains("step-two-show");
+
+let stepThree = document.getElementById("step-three-show");
+let isStepThree  = stepThree.classList.contains("step-three-show");
+
+function steps() {
+    if(isStepOne) {
+        document.querySelectorAll(".step-one-done").forEach(a=>a.style.display = "initial");
+        document.querySelectorAll(".step-one-show").forEach(a=>a.style.display = "none");
+        document.querySelectorAll(".step-two-show").forEach(a=>a.style.display = "initial");
+    } else if(isStepTwo) {
+        document.querySelectorAll(".step-two-done").forEach(a=>a.style.display = "initial");
+        document.querySelectorAll(".step-two-show").forEach(a=>a.style.display = "none");
+        document.querySelectorAll(".step-three-show").forEach(a=>a.style.display = "initial");
+    } else if(isStepThree) {
+        document.querySelectorAll(".step-three-done").forEach(a=>a.style.display = "initial");
+        document.querySelectorAll(".step-three-show").forEach(a=>a.style.display = "none");
+    }
+}
+
+
+
+let picked = document.getElementById("picked");
+let isPicked = picked.classList.contains("picked");
+
+let shipped = document.getElementById("shipped");
+let isShipped = picked.classList.contains("shipped");
+
+function deliveryMethod() {
+    if(isPicked) {
+        document.querySelectorAll(".shipped").forEach(a=>a.style.display = "initial");
+        document.querySelectorAll(".picked").forEach(a=>a.style.display = "none");
+    } else if (isShipped) {
+        document.querySelectorAll(".picked").forEach(a=>a.style.display = "initial");
+        document.querySelectorAll(".shipped").forEach(a=>a.style.display = "none");      
+    }
 }
