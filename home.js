89221f82d68634a6f9e0334002b2feee06de79e8
Ruojia He <187375532+ruojia-he@users.noreply.github.com>
2024-05-13 15:27:30 +1000

    msg

---
+++ home.js
@@ -0,0 +1,25 @@
+/* side bar */
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById("overlay");
+
+
+function overlayOn() {
+    overlay.style.display = "block";
+    sidebar.classList.add('open');
+}
+
+function overlayOff() {
+    overlay.style.display = "none";
+    sidebar.classList.remove('open');
+}
+
+const closeButton = document.querySelector('.sidebar-close');
+closeButton.addEventListener('click', () => {overlayOff()});
+
+
+/* total-purchase */
+const totalPurchaseBtn = document.getElementById("total-purchase-btn");
+
+function btnActive() {
+    totalPurchaseBtn.classList.add('active');
+}
